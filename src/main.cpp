78baba30bd620e1f4b74f6efb2906adfd0c00143
VakhtangKhelashvili <vakhtang.khelashvili@corsair.com>
2022-11-23 16:43:16 +0100

    partial sum

--- src/main.cpp
+++ src/main.cpp
@@ -2,81 +2,138 @@
 
 #include <iostream>
 #include <thread>
+#include <future>
 #include <algorithm>
 #include <execution>
 #include <cassert>
+#include <sstream>
 
-int counter = 0;
 
-template<typename T, typename F>
-void my_for_each(T&& container, F f)
+template<typename iterator>
+std::vector<std::pair<iterator, iterator>> getDivisions(iterator begin, iterator end, unsigned nDivisions)
 {
-#if 0
-	for (auto& val : container)
-		f(val);
-#else
-	auto begin = container.begin();
-	auto end = container.end();
+	std::vector<std::pair<iterator, iterator>> output;
 
-	for (; begin != end; ++begin)
-		f(*begin);
-#endif
+	size_t d = std::distance(begin, end);
+	size_t chunkSize = d / nDivisions;
 
-}
+	auto chunkBegin = begin;
+	auto chunkEnd = begin;
 
-template<typename It, typename F>
-void my_for_each(It begin, It end, F f) noexcept
-{
-	auto first = _Get_unwrapped(begin);
-	auto last = _Get_unwrapped(end);
-	for (; first != last; ++first)
-		f(*first);
+	while (chunkEnd != end) {
+		chunkBegin = chunkEnd;
+		if (d - std::distance(begin, chunkEnd) < chunkSize * 1.1)
+			chunkEnd = end;
+		else
+			std::advance(chunkEnd, chunkSize);
+		output.emplace_back(std::pair<iterator, iterator>(chunkBegin, chunkEnd));
 
-}
-
-template <class _InIt, class _Fn>
-constexpr void std_for_each(_InIt _First, _InIt _Last, _Fn _Func) { // perform function for each element [_First, _Last)
-	_Adl_verify_range(_First, _Last);
-	auto _UFirst = _Get_unwrapped(_First);
-	const auto _ULast = _Get_unwrapped(_Last);
-	for (; _UFirst != _ULast; ++_UFirst) {
-		_Func(*_UFirst);
 	}
+
+	return output;
+}
+
+std::mutex m;
+
+template<typename inputIt, typename outputIt>
+void my_partial_sum(inputIt begin, inputIt end, outputIt outBegin)
+{
+	typename std::iterator_traits<inputIt>::value_type sum = *begin;
+	*outBegin = sum;
+
+	while (++begin != end) {
+		sum = std::move(sum) + *begin;
+		*++outBegin = sum;
+	}
+
+	
+}
+
+template<typename inputIt, typename outputIt>
+void sub_partial_sum(inputIt begin, inputIt end, outputIt outBegin, std::promise<typename std::iterator_traits<inputIt>::value_type> lastVal)
+{
+	using valType = std::iterator_traits<inputIt>::value_type;
+	valType sum = *begin;
+	*outBegin = sum;
+
+	while (++begin != end) {
+		sum = std::move(sum) + *begin;
+		*++outBegin = sum;
+	}
+
+	lastVal.set_value(sum);
+}
+
+template<typename inputIt>
+void increment_by_val(inputIt begin, inputIt end, typename std::iterator_traits<inputIt>::value_type value)
+{
+	for (; begin != end; ++begin)
+		*begin += value;
+}
+
+template<typename inputIt, typename outputIt>
+void partial_sum_increment(inputIt begin, inputIt end, outputIt outBegin, std::promise<typename std::iterator_traits<inputIt>::value_type> outValue, std::promise<typename std::iterator_traits<inputIt>::value_type> incValue)
+{
+	sub_partial_sum<inputIt, outputIt>(begin, end, outBegin, outValue);
+
+	auto future = incValue.get_future();
+	increment_by_val(begin, end, future.get());
+}
+
+/* Parallel partial sum
+* 
+* 1. Divide array in subarrays
+* 2. Do partial sum on each of them concurrently
+*	2.1 partial sum each sub array
+*	2.2 If there are subarrays before current
+* 3. increment values of subarray buy last value of previous sub array if applicable
+* 
+*/
+
+template<typename inputIt, typename outputIt>
+void p_partial_sum(inputIt begin, inputIt end, outputIt outBegin)
+{
+	int nThreads = 4;
+	// Divide Array into subarrays
+	auto chunks = getDivisions(begin, end, nThreads);
+
+	using valType = std::iterator_traits<inputIt>::value_type;
+
+	std::vector<std::promise<valType>> increments(chunks.size() + 1);
+
+	std::vector<std::thread> threads;
+	threads.reserve(chunks.size());
+
+	for (int i = 0; i < chunks.size(); ++i) {
+#if 1
+		threads.emplace_back(partial_sum_increment<inputIt, outputIt>, chunks[i].first, chunks[i].second, outBegin + std::distance(begin, chunks[i].first), increments[i+1], increments[i]);
+#else // bug here 
+		threads.push_back([&]() {
+			sub_partial_sum<inputIt, outputIt>(chunks[i].first, chunks[i].second, outBegin + std::distance(begin, chunks[i].first), increments[i]);
+			});
+#endif
+	}
+
+	for (auto& t : threads)
+		t.join();
+
 }
 
 
 int main()
 {
-	std::vector<int> nums1(100'000'000);
 
-	auto function = [](int i) { i % 2; };
+	std::vector<int> in = { 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1 };
+	auto out = in;
 
-	
-	for (auto& n : nums1)
-		n = random(0, 100);
+	p_partial_sum(in.begin(), in.end(), out.begin());
 
-	for (int i = 0; i < 23; ++i)
-		std::cout << nums1[i] << ' ';
+	for (auto& i : out)
+		std::cout << i << ' ';
 	std::cout << '\n';
 
-	auto nums2 = nums1;
-
-	Timer t1;
-	t1.set();
-	std::for_each(nums2.begin(), nums2.end(), function);
-	t1.stop();
-
-	std::cout << "Elapsed time std [ns]: " << t1.elapsed_ns() << '\n';
-
-	Timer t2;
-	t2.set();
-	my_for_each(nums1.begin(), nums1.end(), function);
-	t2.stop();
-
-	std::cout << "Elapsed time my [ns]: " << t2.elapsed_ns() << '\n';
-
-	for (int i = 0; i < nums1.size(); ++i)
-		assert(nums1[i] == nums2[i]);
-
-
+	std::promise<int> x;
+	sub_partial_sum(in.begin(), in.end(), out.begin(), std::move(x));
+	
+	std::cout << "XX " << x.get_future().get();
 }
